6f2714b65f89c466656de517cc3acb99153f0369
Edward Knyshov <edvorg@gmail.com>
2014-07-20 20:00:27 +0400

    eye antigrav

--- jni/App.hpp
+++ jni/App.hpp
@@ -82,7 +82,7 @@ private:
     const std::vector<float> playerAngleStates { 0.0, -300, -800 };
     const std::vector<float> playerForceStates { 3.25, 0.0, 0.05 };
     const std::vector<float> playerEyeBodyForceStates { 1.0, 1.0, 0.0 };
-    const std::vector<float> playerEyeAntigravStates { 0.0, 0.0, -4.0f };
+    const std::vector<float> playerEyeAntigravStates { 0.0, 0.0, -30.0f };
     const std::vector<float> playerColorR { 0.7, 0.0, 0.6 };
     const std::vector<float> playerColorG { 0.4, 0.7, 0.7 };
     const std::vector<float> playerColorB { 0.4, 1.0, 0.3 };
